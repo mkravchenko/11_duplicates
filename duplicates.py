c1a5a215b9d551850aedf2970c9d9c8cf4107587
mkravchenko <m.kravchenko18@gmail.com>
2016-10-07 15:51:27 +0300

    deleted function for deleting files changed the stucture.

--- duplicates.py
+++ duplicates.py
@@ -1,53 +1,38 @@
 import os
 import sys
 
-map_of_deleted_files = {}
-
 
 def are_files_duplicates(file_path_1, file_path_2):
     if not os.path.exists(file_path_1) and not os.path.exists(file_path_2):
         return None
 
-    map_files_dir1 = get_map_of_files_and_delete_duplicates(file_path_1)
-    map_files_dir2 = {}
-    for root, _, files in os.walk(file_path_2):
-        for name in files:
-            path_file = os.path.join(root, name)
-            size = os.path.getsize(path_file)
-            if name in map_files_dir1.keys():
-                if size == map_files_dir1[name]:
-                    map_of_deleted_files[path_file] = size
-                    os.remove(path_file)
-            elif name in map_files_dir2:
-                if size == map_files_dir2[name]:
-                    map_of_deleted_files[path_file] = size
-                    os.remove(path_file)
-            else:
-                map_files_dir2[name] = size
+    map_files_dir = {}
+    for file_path in [file_path_1, file_path_2]:
+        for root, _, files in os.walk(file_path):
+            for name in files:
+                path_file = os.path.join(root, name)
+                size = os.path.getsize(path_file)
+                naw_name = name + "#" + str(size)
+                if naw_name not in map_files_dir.keys():
+                    map_files_dir[naw_name] = [{"path": path_file}, {"size": size}, []]
+                elif name in map_files_dir.keys() and size != map_files_dir[name][1]["size"]:
+                    map_files_dir[naw_name] = [{"path": path_file}, {"size": size}, []]
+                else:
+                    map_files_dir[naw_name][2].append(path_file)
+    return map_files_dir
 
 
-def get_map_of_files_and_delete_duplicates(file_path):
-    map_files_dir1 = {}
-    for root, _, files in os.walk(file_path):
-        for name in files:
-            path_file = os.path.join(root, name)
-            size = os.path.getsize(path_file)
-            if name not in map_files_dir1.keys():
-                map_files_dir1[name] = size
-            else:
-                map_of_deleted_files[path_file] = size
-                os.remove(path_file)
-    return map_files_dir1
-
-
-def print_duplicated_file_names(list_of_names):
-    total_size = 0
-    if list_of_names:
-        print("Duplicated files was removed(file name, size):\n ")
-        for name, size in list_of_names.items():
-            total_size += size
-            print(name, str(size) + " bates")
-        print("\nTotal size is: {} bytes".format(total_size))
+def print_duplicated_file_names(map_files_dir):
+    if map_files_dir:
+        print("Duplicated files: ")
+        for key, name in map_files_dir.items():
+            if name[2]:
+                file_name = key.split("#")[0]
+                print("\nFile: " + file_name + " with size " + str(name[1]["size"])
+                      + " bates is duplicated:")
+                print(name[0]["path"])
+                for path in name[2]:
+                    print(path)
     else:
         print("Nothing to delete!")
 
@@ -55,5 +40,5 @@ def print_duplicated_file_names(list_of_names):
 if __name__ == '__main__':
     file_path1 = sys.argv[1]
     file_path2 = sys.argv[2]
-    are_files_duplicates(file_path1, file_path2)
-    print_duplicated_file_names(map_of_deleted_files)
+    m = are_files_duplicates(file_path1, file_path2)
+    print_duplicated_file_names(m)
